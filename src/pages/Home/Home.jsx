a387625b34056cbd1d48cdf6aeac80171b5df5fb
abhay963 <abhayyadav96312@gmail.com>
2025-08-09 14:36:54 +0530

    home page done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.png'
+import groot from '../../assets/groot.jpg'
+import narnia from '../../assets/narnia.jpg'
+import jumanji from '../../assets/jumanji.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <div className="hero">
+        <img src={groot} alt="" className='banner-img'/>
+
+        <div className="hero-caption">
+            <img src={hero_title} alt="" className='caption-img' />
+            <p> A ragtag team of intergalactic misfits unites to save the universe from a powerful cosmic threat, all set to an unforgettable mixtape soundtrack.</p>
+
+            <div className="hero-btns">
+                <button className='btn'><img src={play_icon}alt="" />Play</button>
+                <button className='btn dark-btn'><img src={info_icon}alt="" />More Info</button>
+            </div>
+            <TitleCards/>
+        </div>
+      </div>
     </div>
   )
 }
